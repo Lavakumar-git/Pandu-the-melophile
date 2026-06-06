68a2804c7d4a5afbb9a9d6f94e037ea461ef94c1
S Lavakumar <llavakumar90@gmail.com>
2026-06-06 12:28:43 +0530

    intial commit

--- music_web/settings.py
+++ music_web/settings.py
@@ -86,12 +86,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"   # or your provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'groovewaveeditz90@gmail.com'
-EMAIL_HOST_PASSWORD = 'sqhevyoswvappvjx'
+EMAIL_HOST_USER = os.environ.get("groovewaveeditz90@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("sqhevyoswvappvjx")
 
 
 import os
