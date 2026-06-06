8d64d68f7c37b5960f05bdf1525abfbd716beae0
S Lavakumar <llavakumar90@gmail.com>
2026-06-06 11:52:02 +0530

    intial commit

--- music_web/settings.py
+++ music_web/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,3 +92,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'groovewaveeditz90@gmail.com'
 EMAIL_HOST_PASSWORD = 'sqhevyoswvappvjx'
+
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
